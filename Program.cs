251f3667defe1b01ea7981f4d4fd7fe92aed8cf6
Abram <abram.perez795@gmail.com>
2025-10-06 19:59:07 -0500

    [1.07] serializer.deserializer

--- Program.cs
+++ Program.cs
@@ -4,3 +4,97 @@ using System.Reflection;
 using System.Text.Json;
 
 string path = Directory.GetCurrentDirectory() + "//nlog.config";
+
+List<DonkeyKongCharacter> donkeyKongList = new List<DonkeyKongCharacter>();
+List<SF2Character> sf2List = new List<SF2Character>();
+
+if (File.Exists("dk.json"))
+{
+    var result = JsonSerializer.Deserialize<List<DonkeyKongCharacter>>(File.ReadAllText("dk.json"));
+    donkeyKongList = result ?? new List<DonkeyKongCharacter>();
+}
+
+if (File.Exists("sf2.json"))
+{
+    var result = JsonSerializer.Deserialize<List<SF2Character>>(File.ReadAllText("sf2.json"));
+    sf2List = result ?? new List<SF2Character>();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
